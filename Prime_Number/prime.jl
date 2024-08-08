3b06d6de42bf0200f3d62a10a85cc65b8a460f9c
William Rios <133013480+Winnd11@users.noreply.github.com>
2024-08-08 18:26:26 -0300

    create primeFactor function

--- Prime_Number/prime.jl
+++ Prime_Number/prime.jl
@@ -7,3 +7,15 @@ function isPrime(x)
 end
 
 isPrime(39) #  if isPrime only return 2 numbers that means it is a prime number
+
+function primeFactor(x)
+    for i in 1:x
+        result = x % i
+        if result == 0
+            x = x / i
+            println(i)
+        end
+    end
+end
+
+primeFactor(13195)
