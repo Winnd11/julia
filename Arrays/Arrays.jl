d5ff48accc8e306c4c4a01cdfae61a0d0811a573
William Rios <133013480+wiirios@users.noreply.github.com>
2024-11-26 15:39:51 -0300

    Update Arrays.jl

--- Arrays/Arrays.jl
+++ Arrays/Arrays.jl
@@ -1,7 +1,20 @@
 persons = ["Davi", "William"]
+numbers = [10, 20]
 
 # in julia the first index of array is 1
 # in java for example the first index of array is 0
 println(persons[1])
 
-println(typeof(persons))
+# Arrays are mutable
+persons[1] = "Julia"
+println(persons)
+
+# julia> typeof(persons)
+# Vector{String} (alias for Array{String, 1})
+
+# julia> typeof(numbers)
+# Vector{Int64} (alias for Array{Int64, 1})
+
+for i in eachindex(persons)
+    println(persons(i))
+end
